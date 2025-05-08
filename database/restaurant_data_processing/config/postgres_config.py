457c151a77df70e66b803a12ad5d20326c5335b7
NP10t <NP10t@gmail.com>
2025-05-09 00:04:48 +0700

    Add deduplicate feature

--- database/restaurant_data_processing/config/postgres_config.py
+++ database/restaurant_data_processing/config/postgres_config.py
@@ -1,9 +1,11 @@
+import os
+
 def get_postgres_config():
     return {
         "url": "jdbc:postgresql://db:5432/restaurants",
         "properties": {
-            "user": "postgres",
-            "password": "example",
+            "user": os.getenv("POSTGRES_USER"),
+            "password": os.getenv("POSTGRES_PASSWORD"),
             "driver": "org.postgresql.Driver",
             "stringtype": "unspecified"
         }
